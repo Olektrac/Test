fc43ea6bf27a9f45181707589c487253b46fc5fe
SamJM <trachuk23232323@gmail.com>
2023-06-10 10:25:44 +0200

    10:25

--- Lection2/Lection2.cpp
+++ Lection2/Lection2.cpp
@@ -2,15 +2,65 @@
 #include <string>
 using namespace std;
 
+
+class Interests
+{
+public:
+	Interests(); // Constructor default
+	Interests(string thing); // Constructor with param
+	~Interests(); // Destructor
+	string getThing(); // Getter
+	void setThing(); // Setter 
+private:
+	string thing;
+};
+
+class User
+{
+public:
+	User()
+	{
+		cout << "Constructor default: " << typeid(this).name() << endl;
+	}
+private:
+	int _id;
+	string _name;
+	Interests* _interests;
+	int _size;
+};
+
+
 int main()
 {
 	
-	cout << "Hello, Git!" << endl;
-	for (size_t i = 0; i < 15; i++)
-	{
-		cout << "Hello, Git!" << endl;
-	}
+
+
 
 	return 0;
 }
 
+Interests::Interests()
+{
+	cout << "Construcor default: " << typeid(this).name() << endl;
+}
+
+Interests::Interests(string thing)
+{
+	this->thing = thing;
+	cout << "Construcor with param: " << typeid(this).name() << endl;
+}
+
+Interests::~Interests()
+{
+	cout << "Destructor: " << typeid(this).name() << endl;
+}
+
+string Interests::getThing()
+{
+	return thing;
+}
+
+void Interests::setThing()
+{
+	this->thing = thing;
+}
